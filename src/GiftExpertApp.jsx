4b6035c82e65e0e3d62b27ed8e96c27b25d4a8e0
Eliud Muñoz <eliud.munoz1255@alumnos.udg.mx>
2022-07-19 18:35:03 -0500

    First commit

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,35 @@
+import {useState} from 'react'
+import { AddCategory, GifGrid} from './components';
+
+export const GiftExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch']);
+
+  const onAddCategory = (newCategory) =>{
+    if(categories.includes(newCategory)) return;
+    
+    setCategories([...categories, newCategory]);
+  }
+
+  return (
+    <>
+
+        <h1>GiftExpertApp</h1>
+
+        <AddCategory 
+          onNewCategory={event => onAddCategory(event)}
+        />
+
+        {
+        categories.map(category => 
+            (
+              <GifGrid 
+                key={category} 
+                category ={category}
+                />
+            ))
+        }
+
+    </>
+  )
+}
